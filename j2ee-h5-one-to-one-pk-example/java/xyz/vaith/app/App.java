8758fd2b23af9ea4e7b4dc1f85a0bd6294554f10
Vaith <vaithwee@yeah.net>
2019-03-19 16:43:04 +0800

    Add hibernate one to one pk example

---
+++ j2ee-h5-one-to-one-pk-example/java/xyz/vaith/app/App.java
@@ -0,0 +1,25 @@
+package xyz.vaith.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import xyz.vaith.app.domain.Address;
+import xyz.vaith.app.domain.Person;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        Person person = new Person();
+        person.setAge(1);
+        person.setName("wee");
+        Address address = new Address();
+        address.setDetail("Fz");
+        address.setPerson(person);
+        session.save(address);
+        transaction.commit();
+    }
+}
