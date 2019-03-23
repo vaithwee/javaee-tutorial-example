4ff64f59a79965c48ce008f7c0a9abe746ac535f
Vaith <vaithwee@yeah.net>
2019-03-23 11:28:57 +0800

    Add spring p namespace injection example

---
+++ j2ee-s5-p-injection-example/java/xyz/vaith/app/domain/Student.java
@@ -0,0 +1,40 @@
+package xyz.vaith.app.domain;
+
+public class Student {
+    private String name;
+    private String age;
+    private School school;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", school=" + school +
+                '}';
+    }
+}
