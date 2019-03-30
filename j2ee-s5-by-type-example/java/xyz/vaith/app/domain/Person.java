618405766b2d025b79cacf317991c3ca545b4d2c
Vaith <vaithwee@yeah.net>
2019-03-30 18:59:32 +0800

    Added spring 5 autowire by type example

---
+++ j2ee-s5-by-type-example/java/xyz/vaith/app/domain/Person.java
@@ -0,0 +1,30 @@
+package xyz.vaith.app.domain;
+
+public class Person {
+    private String name;
+    private Dog dog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", dog=" + dog +
+                '}';
+    }
+}
